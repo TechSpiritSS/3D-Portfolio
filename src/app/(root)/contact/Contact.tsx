54ca47db5a9d868bfbc4a3d37772dac523a76a7a
Sidharth Sethi <Sidharth.sherry@gmail.com>
2024-02-01 23:30:10 +0530

    Contact Page Complete

---
+++ src/app/(root)/contact/Contact.tsx
@@ -0,0 +1,174 @@
+// @ts-nocheck
+
+'use client';
+
+import Alert from '@/components/Alert';
+import Loader from '@/components/Loader';
+import useAlert from '@/hooks/useAlert';
+import Fox from '@/models/Fox';
+import emailjs from '@emailjs/browser';
+import { Canvas } from '@react-three/fiber';
+import { ChangeEvent, Suspense, useRef, useState } from 'react';
+
+const Contact = () => {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [currentAnimation, setCurrentAnimation] = useState('idle');
+  const [isLoading, setIsLoading] = useState(false);
+  const formRef = useRef(null);
+  const { alert, showAlert, hideAlert } = useAlert();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setCurrentAnimation('hit');
+
+    emailjs
+      .send(
+        process.env.NEXT_PUBLIC_APP_EMAILJS_SERVICE_ID,
+        process.env.NEXT_PUBLIC_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: 'Sidharth Sethi',
+          from_email: form.email,
+          to_email: 'techspiritss@duck.com',
+          message: form.message,
+        },
+        process.env.NEXT_PUBLIC_APP_EMAILJS_PUBLICKEY
+      )
+      .then(() => {
+        showAlert({
+          show: true,
+          text: 'Message sent successfully!',
+          type: 'success',
+        });
+      })
+      .catch(() => {
+        showAlert({
+          show: true,
+          text: 'Something went wrong!',
+          type: 'danger',
+        });
+      })
+      .finally(() => {
+        setIsLoading(false);
+        setCurrentAnimation('idle');
+        setForm({
+          name: '',
+          email: '',
+          message: '',
+        });
+
+        formRef.current.reset();
+
+        setTimeout(() => {
+          hideAlert();
+        }, 3000);
+      });
+  };
+  const handleFocus = () => {
+    setCurrentAnimation('walk');
+  };
+  const handleBlur = () => {
+    setCurrentAnimation('idle');
+  };
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert} />}
+
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch</h1>
+        <form
+          className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
+          ref={formRef}
+        >
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Sidharth"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="techspiritss@duck.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Message
+            <input
+              type="text"
+              name="message"
+              className="input"
+              placeholder="Enter your message..."
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <button
+            type="submit"
+            className="btn"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            disabled={isLoading}
+          >
+            {isLoading ? 'Sending...' : 'Send Message'}
+          </button>
+        </form>
+      </div>
+      <div className="lg:w-1/2 w-full lg:h-auto md:h=[550px] h=[350px]">
+        <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000,
+          }}
+        >
+          <directionalLight intensity={2.5} position={[0, 0, 1]} />
+          <ambientLight intensity={0.5} />
+          <Suspense fallback={<Loader />}>
+            <Fox
+              currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.625, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/app/(root)/contact/page.tsx
+++ src/app/(root)/contact/page.tsx
@@ -1,5 +1,7 @@
+import Contact from './Contact';
+
 const page = () => {
-  return <div>page</div>;
+  return <Contact />;
 };
 
 export default page;
